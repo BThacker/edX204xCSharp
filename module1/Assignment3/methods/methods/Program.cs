6bbaae963b3cabfd638883b5ce63e3622db8b409
BThacker <b.thacker@outlook.com>
2015-04-15 19:35:36 -0400

    Assignment3and4
    Completed

---
+++ module1/Assignment3/methods/methods/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            GetStudentInfo();
+            GetSchoolInfo();
+            ValidateStudBirthday();
+        }
+        static void GetStudentInfo()
+        {
+            Console.WriteLine("Enter the student's first name: ");
+            string sfirstName = Console.ReadLine();
+            Console.WriteLine("Enter the student's middle name: ");
+            string smiddleName = Console.ReadLine();
+            Console.WriteLine("Enter the student's last name");
+            string slastName = Console.ReadLine();
+            Console.WriteLine("Enter the student's birthday mm/dd/yyyy");
+            string sbirthDate = Console.ReadLine();
+            PrintStudentDetails(sfirstName, smiddleName, slastName, sbirthDate);
+        }
+        static void GetSchoolInfo()
+        {
+            Console.WriteLine("Enter the teachers's first name: ");
+            string tfirstName = Console.ReadLine();
+            Console.WriteLine("Enter the teachers's middle name: ");
+            string tmiddleName = Console.ReadLine();
+            Console.WriteLine("Enter the teachers's last name");
+            string tlastName = Console.ReadLine();
+            Console.WriteLine("Enter the teachers years of experience: ");
+            string tYearsExp = Console.ReadLine();
+            Console.WriteLine("Enter the course name: ");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("Enter the program name: ");
+            string programName = Console.ReadLine();
+            Console.WriteLine("Enter the degree name ");
+            string degreeName = Console.ReadLine();
+            PrintTeacherDetails(tfirstName, tmiddleName, tlastName, tYearsExp);
+            PrintProgramDetails(courseName, programName, degreeName);
+        }
+        static void ValidateStudBirthday()
+        {
+            throw new NotImplementedException();
+        }
+       
+        static void PrintStudentDetails(string first, string middle, string last, string birthday)
+        {
+            Console.WriteLine("{0} {1} {2} was born on: {3}", first, middle, last, birthday);
+        }
+        static void PrintTeacherDetails(string first, string middle, string last, string yearsExp)
+        {
+            Console.WriteLine("{0} {1} {2} has been teaching for {3} years.", first, middle, last, yearsExp);
+        }
+        static void PrintProgramDetails(string course, string program, string degree)
+        {
+            Console.WriteLine("The course is {0}. The program is: {1}. The degree will be: {2}", course, program, degree);
+        }
+    }
+}
---
+++ module1/Assignment4/Arrays/Arrays/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student brandon = new Student();
+            brandon.firstName = "Brandon";
+            brandon.lastName = "LastName";
+            brandon.age = 28;
+
+            Student[] studArray = new Student[5];
+            studArray[0] = new Student();
+            studArray[1] = new Student();
+            studArray[2] = new Student();
+            studArray[3] = new Student();
+            studArray[4] = new Student();
+
+            studArray[0].firstName = "TestingArrayName";
+            Console.WriteLine(brandon.firstName);
+            Console.WriteLine(brandon.lastName);
+            Console.WriteLine(brandon.age);
+            Console.WriteLine(studArray[0].firstName);
+
+        }
+    }
+    public struct Student
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+    }
+    public struct Teacher
+    {
+        public string firstName;
+        public string lastName;
+        public int yearsTeaching;
+    }
+    public struct ProgramDetails
+    {
+        public string name;
+    }
+    public struct Course
+    {
+        public string name;
+    }
+    public struct Degree
+    {
+        public string name;
+        public int yearsLeft;
+    }
+}
